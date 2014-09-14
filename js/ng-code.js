136678f35f1af0727c7d38688549417aa122298a
unknown <sagar.chandarana123@gmail.com>
2014-09-14 19:48:58 -0300

    add vertex: in root and a namespace

--- js/ng-code.js
+++ js/ng-code.js
@@ -123,54 +123,68 @@ angular.module("abDataBrowser", ['ngAppbase', 'ngRoute', 'ng-breadcrumbs', 'ngDi
     $scope.addEdgeInto = function(node) {
       ngDialog.open({
         template: 'html/dialog-small.html',
-        controller: function($scope) {
-          $scope.title = "Add Out-vertex at path: " + node.$ref.$path();
-          $scope.text = "in " + node.name
-          $scope.vTypeOptions = ['New Vertex', 'Existing Vertex']
-          $scope.vType = $scope.vTypeOptions[0]
-          $scope.namespaceN = 'asdkj'
+        controller: ['$scope', function($dialogScope) {
+          $dialogScope.node = node
+          if (!node.isV) {
+            $dialogScope.title = "Add Vertex"
+          } else {
+            $dialogScope.title = "Add Out-vertex at path: " + node.$ref.$path();
+          }
+          if(node.isNS) {
+            $dialogScope.namespaceSelected = node.name
+          }
+          $dialogScope.text = "in " + node.name
+          $dialogScope.vTypeOptions = ['New Vertex', 'Existing Vertex']
+          $dialogScope.vType = $dialogScope.vTypeOptions[0]
+          $dialogScope.namespaceN = 'asdkj'
 
-          $scope.namespaces = ['Loading..']
+          $dialogScope.namespaces = ['Loading..']
           data.getNamespaces(function(array) {
             $timeout(function() {
-              $scope.namespaces = array
+              $dialogScope.namespaces = array
             })
           })
 
           //prevents user from choosing 'Loading..'
-          $scope.$watch( function() {
-            return $scope.namespaceSelected
+          $dialogScope.$watch( function() {
+            return $dialogScope.namespaceSelected
           }, function(val) {
-            $scope.namespaceSelected = (val === 'Loading..' ? null : val)
+            $dialogScope.namespaceSelected = (val === 'Loading..' ? null : val)
           })
 
-          $scope.done = function() {
+          $dialogScope.done = function() {
             var prepareParams = function() {
               var params = {}
-              if($scope.vType === $scope.vTypeOptions[0]) { // New Vertex
-                params.namespace = ($scope.namespaceSelected === undefined || $scope.namespaceSelected === null) ? $scope.namespaceNew : $scope.namespaceSelected
-                params.vId = ($scope.vId === undefined || $scope.vId === "") ? Appbase.uuid() : $scope.vId
+              if($dialogScope.vType === $dialogScope.vTypeOptions[0]) { // New Vertex
+                params.namespace = ($dialogScope.namespaceSelected === undefined || $dialogScope.namespaceSelected === null) ? $dialogScope.namespaceNew : $dialogScope.namespaceSelected
+                params.vId = ($dialogScope.vId === undefined || $dialogScope.vId === "") ? Appbase.uuid() : $dialogScope.vId
                 params.ref = $appbaseRef(Appbase.create(params.namespace, params.vId))
               } else {
-                params.vPath = $scope.vPath
+                params.vPath = $dialogScope.vPath
                 params.ref = $appbaseRef(params.vPath)
               }
 
-              params.eName = ($scope.eName === undefined || $scope.eName === "") ? (params.vId === undefined? Appbase.uuid() : params.vId) : $scope.eName
-              params.pR = ($scope.pR === undefined || $scope.pR === null) ? undefined : $scope.pR
+              params.eName = ($dialogScope.eName === undefined || $dialogScope.eName === "") ? (params.vId === undefined? Appbase.uuid() : params.vId) : $dialogScope.eName
+              params.pR = ($dialogScope.pR === undefined || $dialogScope.pR === null) ? undefined : $dialogScope.pR
 
               return params
             }
 
             var params = prepareParams()
-            node.$ref.$setEdge(params.ref, params.eName, params.pR)
-            $scope.closeThisDialog()
+            if(node.isV) {
+              node.$ref.$setEdge(params.ref, params.eName, params.pR)
+            } else if(node.isNS) {
+              node.children.unshift(nodeBinding.bindAsVertex($scope, params.namespace + '/' + params.vId))
+            } else {
+              node.children.unshift(nodeBinding.bindAsNamespace($scope, params.namespace))
+            }
+            $dialogScope.closeThisDialog()
           }
 
-          $scope.no = function() {
-            $scope.closeThisDialog()
+          $dialogScope.no = function() {
+            $dialogScope.closeThisDialog()
           }
-        },
+        }],
         className: 'ngdialog-theme-dialog-small',
         showClose: false
       })
