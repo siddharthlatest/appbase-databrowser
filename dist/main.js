7b4f9a5c0641e96be4548833da08f732a3337ebf
Henrique Sa <henriquepss@gmail.com>
2015-02-23 17:13:17 -0600

    Fixed pagination previous page bug

--- dist/main.js
+++ dist/main.js
@@ -1577,7 +1577,7 @@ function NodeBinding(data, stringManipulation, $timeout, $appbase, $rootScope, s
     vertex.prevPage = function(){
       if(vertex.page > 0) {
         vertex.page--;
-        vertex.children = [];
+        // vertex.children = [];
         vertex.expand();
       }
     }
